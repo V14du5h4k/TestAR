bef331eaf94c4a3d942ec1959902df992b520323
TaxaRun <taxarun@gmail.com>
2017-11-08 10:06:46 +0300

    First version

---
+++ TestAR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAR
+//
+//  Created by Robert Enderson on 08.11.2017.
+//  Copyright © 2017 Ермаков Николай. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestAR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAR
+//
+//  Created by Robert Enderson on 08.11.2017.
+//  Copyright © 2017 Ермаков Николай. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
